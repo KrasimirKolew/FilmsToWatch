ed55e6966123c0a3c6b6fea299244a8de0e92564
Krasimir Kolev <krasirumenov789@gmail.com>
2024-03-22 14:03:47 +0200

    clean login and register

--- FilmsToWatch/Program.cs
+++ FilmsToWatch/Program.cs
@@ -10,8 +10,15 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 	options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-	.AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+{
+	//todo after project finish fix paslords !!!!!!!!!!!!!!!!!!!!!!!!!!!
+    options.SignIn.RequireConfirmedAccount = false;
+    options.Password.RequireDigit = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+})
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
