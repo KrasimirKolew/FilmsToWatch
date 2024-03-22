04370eea2640693cb09e08e37140e9e36a9b056a
Krasimir Kolev <krasirumenov789@gmail.com>
2024-03-22 16:31:43 +0200

    add 2 controlers

---
+++ FilmsToWatch/Controllers/GenreController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FilmsToWatch.Controllers
+{
+    public class GenreController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
