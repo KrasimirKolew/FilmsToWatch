da4a653b1e33e1d7f933f3792ff2f67c7ab7697a
Krasimir Kolev <krasirumenov789@gmail.com>
2024-03-23 18:12:54 +0200

    add actor model

---
+++ FilmsToWatch/Data/Models/Actor.cs
@@ -0,0 +1,11 @@
+﻿namespace FilmsToWatch.Data.Models
+{
+    public class Actor
+    {
+        public int Id { get; set; }
+
+        public string ActorName { get; set; } = string.Empty;
+
+        public int FilmsInvolve { get; set; }
+    }
+}
