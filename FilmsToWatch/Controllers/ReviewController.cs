da38ac887ec4f0a00ba98abae168e4fc6960d709
Krasimir Kolev <krasirumenov789@gmail.com>
2024-03-24 15:35:15 +0200

    add review controller

---
+++ FilmsToWatch/Controllers/ReviewController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FilmsToWatch.Controllers
+{
+    public class ReviewController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
