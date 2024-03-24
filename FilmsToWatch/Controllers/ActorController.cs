8f2fec589d5a12559b0662dc6b8c85870f851fef
Krasimir Kolev <krasirumenov789@gmail.com>
2024-03-24 15:02:08 +0200

    add Actor controller

---
+++ FilmsToWatch/Controllers/ActorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FilmsToWatch.Controllers
+{
+    public class ActorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
