f063f2f59c8ebce22932c92098ad6871dd2b626e
Krasimir Kolev <krasirumenov789@gmail.com>
2024-04-08 15:39:39 +0300

    FilmFormModel add string len

--- FilmsToWatch/Models/FilmModels/FilmFormModel.cs
+++ FilmsToWatch/Models/FilmModels/FilmFormModel.cs
@@ -1,27 +1,39 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using static FilmsToWatch.Constants.DataConstants;
 
 namespace FilmsToWatch.Models.FilmModels
 {
     public class FilmFormModel
     {
-        [Required]
 
+        [Required(ErrorMessage = RequiredMesage)]
+        [StringLength(TitleMaxLen,
+            MinimumLength = TitleMinLen,
+            ErrorMessage = LenghtMessage)]
         public string Title { get; set; } = string.Empty;
+
 
         public string MovieImage { get; set; } = string.Empty;
 
-        [Required]
+
+        [Required(ErrorMessage = RequiredMesage)]
         public string ReleaseYear { get; set; } = string.Empty;
 
-        [Required]
 
+        [Required(ErrorMessage = RequiredMesage)]
+        [StringLength(DirectorMaxLen,
+            MinimumLength = DirectorMinLen,
+            ErrorMessage = LenghtMessage)]
         public string Director { get; set; } = string.Empty;
 
-        [Required]
+
+        [Required(ErrorMessage = RequiredMesage)]
         public int GenreId { get; set; }
 
-        [Required]
+
+        [Required(ErrorMessage = RequiredMesage)]
         public int ActorId { get; set; }
+
 
         public IEnumerable<FilmGenreServiceModel> Genres { get; set; } =
             new List<FilmGenreServiceModel>();
