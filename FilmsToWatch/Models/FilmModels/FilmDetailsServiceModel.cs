0e10c329e3524c39943b8721c60ad08e48dd41c1
Krasimir Kolev <krasirumenov789@gmail.com>
2024-04-12 20:09:42 +0300

    add model view

---
+++ FilmsToWatch/Models/FilmModels/FilmDetailsServiceModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmsToWatch.Models.FilmModels
+{
+    public class FilmDetailsServiceModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+        
+        public string Genre { get; set; } = string.Empty;
+
+        public string Actor { get; set; } = string.Empty;
+
+        public string MovieImage { get; set; } = string.Empty;
+
+        public string ReleaseYear { get; set; } = string.Empty;
+
+        public string Director { get; set; } = string.Empty;
+    }
+}
