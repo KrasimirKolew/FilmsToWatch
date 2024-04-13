ed2a6f0d8c69b093f66e800ba4f27d7966914217
Krasimir Kolev <krasirumenov789@gmail.com>
2024-04-13 12:26:18 +0300

    add review model

---
+++ FilmsToWatch/Models/ReviewModels/ReviewViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static FilmsToWatch.Constants.DataConstants;
+
+namespace FilmsToWatch.Models.ReviewModels
+{
+    public class ReviewViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = RequiredMesage)]
+        [StringLength(ContentMaxLen,
+            MinimumLength = ContentMinLen,
+            ErrorMessage = LenghtMessage)]
+        public string Content { get; set; } = string.Empty;
+    }
+}
