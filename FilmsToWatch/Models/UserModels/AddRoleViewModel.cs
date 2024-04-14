28f6c7008b0c851c019d1ade38712f1b0506e28a
Krasimir Kolev <krasirumenov789@gmail.com>
2024-04-14 20:43:58 +0300

    add view model for ading a role

---
+++ FilmsToWatch/Models/UserModels/AddRoleViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static FilmsToWatch.Constants.DataConstants;
+
+namespace FilmsToWatch.Models.UserModels
+{
+    public class AddRoleViewModel
+    {
+        [Required]
+        [StringLength(UserRoleNameMaxLenght, MinimumLength = UserRoleNameMinLenght, ErrorMessage = LenghtMessage)]
+        public string RoleName { get; set; } = string.Empty;
+    }
+}
