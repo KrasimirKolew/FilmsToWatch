b3c0d0a474f536f24225f49f77a622eb3c444d0c
Krasimir Kolev <krasirumenov789@gmail.com>
2024-04-14 20:45:04 +0300

    add users service logic

---
+++ FilmsToWatch/Repositories/Contracts/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace FilmsToWatch.Repositories.Contracts
+{
+    public interface IUserService
+    {
+        IdentityRole CreateRole(string roleName);
+    }
+}
---
+++ FilmsToWatch/Repositories/Services/UserService.cs
@@ -0,0 +1,14 @@
+﻿using FilmsToWatch.Repositories.Contracts;
+using Microsoft.AspNetCore.Identity;
+
+namespace FilmsToWatch.Repositories.Services
+{
+    public class UserService : IUserService
+    {
+        public IdentityRole CreateRole(string roleName) => new IdentityRole()
+        {
+            Name = roleName,
+            NormalizedName = roleName.ToUpper()
+        };
+    }
+}
