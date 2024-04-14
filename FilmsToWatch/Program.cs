a8fe0b538824a0916f45464cb14ad32adf29f83c
Krasimir Kolev <krasirumenov789@gmail.com>
2024-04-14 13:37:15 +0300

    add error handling

--- FilmsToWatch/Program.cs
+++ FilmsToWatch/Program.cs
@@ -38,12 +38,13 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+	app.UseDeveloperExceptionPage();
 	app.UseMigrationsEndPoint();
 }
 else
 {
-	app.UseExceptionHandler("/Home/Error");
-	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseExceptionHandler("/Home/Error/500");
+	app.UseStatusCodePagesWithRedirects("/Home/Error?statusCode={0}");
 	app.UseHsts();
 }
 
