17304e1e4535c54476b966558d540ca70afe8483
Krasimir Kolev <krasirumenov789@gmail.com>
2024-04-15 21:18:42 +0300

    add unit test

---
+++ FilmsToWatch.UnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace FilmsToWatch.UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
