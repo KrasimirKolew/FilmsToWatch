f022ad98ab407f0ca9dc33770d888e29f6dcaa0c
Krasimir Kolev <krasirumenov789@gmail.com>
2024-04-16 14:08:36 +0300

    user test

---
+++ FilmsToWatch.UnitTests/UserServiceTests.cs
@@ -0,0 +1,52 @@
+﻿using FilmsToWatch.Data;
+using FilmsToWatch.Repositories.Contracts;
+using FilmsToWatch.Repositories.Services;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmsToWatch.UnitTests
+{
+    [TestFixture]
+    public class UserServiceTests
+    {
+        private IUserService userService;
+        private ApplicationDbContext context;
+
+        [SetUp]
+        public void Setup()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase("TestDataBase")
+                .Options;
+
+            context = new ApplicationDbContext(options);
+
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+        }
+
+        [Test]
+        public void CreateRole_ReturnsRoleWithCorrectProperties()
+        {
+            userService = new UserService();
+
+            var roleName = "Admin";
+
+            var role = userService.CreateRole(roleName);
+
+            Assert.IsNotNull(role);
+            Assert.That(role.Name, Is.EqualTo(roleName));
+            Assert.That(role.NormalizedName, Is.EqualTo(roleName.ToUpper()));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            context.Dispose();
+        }
+    }
+}
